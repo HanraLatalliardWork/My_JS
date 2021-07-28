5a3fae0409ca0ee75d801525ad67c3596ac20e95
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-28 19:24:03 +0200

    trying to fix bugs

--- files/js/ExternalPermanentScripts.js
+++ files/js/ExternalPermanentScripts.js
@@ -10,7 +10,7 @@ function AddScripts(IDTo){
     TheScripts+='    <script type="text/JavaScript" src="'+homeScripts+'files/js/dark_light.js"></script>\n';
     TheScripts+='    <script type="text/JavaScript" src="'+homeSite+'files/customisation/js/head.js"></script>\n';
     TheScripts+='    <script type="text/JavaScript" src="'+homeSite+'files/customisation/js/BodyHeader.js"></script>\n';
-    TheScripts+='    <script type="text/JavaScript" src="'+homeScripts+'files/js/apply_d_l_from_theme.js"></script>\n';
+    // TheScripts+='    <script type="text/JavaScript" src="'+homeScripts+'files/js/apply_d_l_from_theme.js"></script>\n';
     console.log("TheScripts has been loaded with info.\n e='"+e+"'")
     e+=TheScripts;
     document.getElementById(IDTo).innerHTML=e;
