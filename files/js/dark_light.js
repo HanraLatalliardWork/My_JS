b7c1a895cbf55ae0b0a83e664e4e057eb735d74d
Hanra <37299738+HenraL@users.noreply.github.com>
2021-08-20 01:48:45 +0200

    Update dark_light.js

--- files/js/dark_light.js
+++ files/js/dark_light.js
@@ -12,35 +12,46 @@ function theme(ID = "darkOrLight", ID2 = "theme",home="https://hanralatalliardwo
 }
 
 function initialiseTheme(ID,IDRead,home="https://hanralatalliardwork.github.io/wolf_escape_home/",themeD="files/customisation/css/boostrap_dark.css",themeL="files/customisation/css/bootstrap.css") {
+    var url=document.location.href;
     try {
         e=readCookie("theme");
         if (e==="L"){
             document.getElementById(ID).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + themeL +"\">";
             document.getElementById(IDRead).checked=false;
             createCookie("theme","L","","/");
+        } else {
+            console.log("The theme is not light.")
         }
         if (e==="D"){
             document.getElementById(ID).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + themeD+ "\">";
             document.getElementById(IDRead).checked=true;
             createCookie("theme","D","","/");
+        } else {
+            console.log("The theme is not dark.")
         }
         if (e===""){
             document.getElementById(ID).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + themeL+"\">";
             document.getElementById(IDRead).checked=false;
             createCookie("theme","L","","/");
+        } else {
+            console.log("There is a theme.")
         }
     } catch(err) {
         document.getElementById(ID).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + themeL+"\">";
         // document.cookie="theme=L";
         createCookie("theme","L","","/");
     }
-    try{
-        console.log("running checkTheme");
-        checkTheme("theme","DownloadLogo","DownloadButtonCredits","DiscordLogo");
-        console.log("checkTheme has been run successfully");
-    } catch(err) {
-        console.log("failed to run checkTheme");
-        console.log("Theme Check= "+err);
+    if (url===home){
+        try{
+            console.log("running checkTheme");
+            checkTheme("theme","DownloadLogo","DownloadButtonCredits","DiscordLogo");
+            console.log("checkTheme has been run successfully");
+        } catch(err) {
+            console.log("failed to run checkTheme");
+            console.log("Theme Check= "+err);
+        }
+    } else {
+        console.log("We are not in the home page.")
     }
 }
 // First file
