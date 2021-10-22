220eae1247b72405b605c4088bedfec51bded1be
Hanra <37299738+HenraL@users.noreply.github.com>
2021-10-23 00:11:56 +0200

    Update cookie_dealing.js

--- files/js/cookie_dealing.js
+++ files/js/cookie_dealing.js
@@ -1,4 +1,4 @@
-console.log("My_styling/js/cokkie_dealing initialising");
+console.log("My_styling/js/cookie_dealing initialising");
 var createMany=function(string){
     document.cookie = string
 }
@@ -56,4 +56,4 @@ var readCookie=function(key){ //from https://www.w3schools.com/js/js_cookies.asp
 var removeCookie=function(key){
     document.cookie = key+"=a; expires=Sat, 1 Jan 1921 00:00:00 UTC;"
 }
-console.log("My_styling/js/cokkie_dealing initialised");
+console.log("My_styling/js/cookie_dealing initialised");
